8c68d207bb151828e96715ad57c557d6bee71e3b
jonathancoggin <jonathancoggin@localhost>
2022-09-08 12:36:54 -0500

    Studio 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,16 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		// Booleans that determine whether the order is correct
+		boolean xGreaterY = (x - y) < 0;
+		boolean yGreaterZ = (y - z) < 0;
+		
+		
+		// Assesses whether the whole statement is ordered
+		boolean isOrdered = xGreaterY && yGreaterZ;
+		
+		System.out.println(isOrdered);
 
 	}
 
